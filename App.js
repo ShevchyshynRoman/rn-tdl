7b7244d30e18d5676c30d232454588df9329847e
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2022-07-14 12:06:11 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
